a4bc73965e5c995d6aecc67563239c766d9cc113
wikky2012 <wicliferono@gmail.com>
2020-09-07 22:44:37 +0300

    test for viewing credentials added

--- user.py
+++ user.py
@@ -20,22 +20,22 @@ class User:
     def delete_credential_account(self):
         User.user_list.remove(self)
 
-# class Credentials:
-#     def __init__(self,username,password):
-#         self.username = username
-#         self.password = password
+class Credentials:
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
 
-#     def save_existing_credential_accounts(self):
-#         User.user_list.append(self)
+    def save_existing_credential_accounts(self):
+        User.user_list.append(self)
 
-#     @classmethod
-#     def view_my_various_credential_accounts(cls,username):
-#         for user in cls.user_list:
-#             if user.username == username:
-#                 return cls.user_list
+    @classmethod
+    def view_my_various_credential_accounts(cls,username):
+        for user in cls.user_list:
+            if user.username == username:
+                return cls.user_list
 
-#     def delete_credential_account(self):
-#         User.user_list.remove(self)
+    def delete_credential_account(self):
+        User.user_list.remove(self)
 
 
     
