3dfb3ee7a6cabfa6d1abd1b5bb75297b3723f0d5
wikky2012 <wicliferono@gmail.com>
2020-09-07 22:40:50 +0300

    add delete test

--- user.py
+++ user.py
@@ -12,26 +12,30 @@ class User:
         User.user_list.append(self)
 
     @classmethod
-    def view_my_various_credential_accounts(cls):
-        return cls.user_list
+    def view_my_various_credential_accounts(cls,username):
+        for user in cls.user_list:
+            if user.username == username:
+                return user
 
     def delete_credential_account(self):
         User.user_list.remove(self)
 
-class Credentials:
-    def __init__(self,username,password):
-        self.username = username
-        self.password = password
+# class Credentials:
+#     def __init__(self,username,password):
+#         self.username = username
+#         self.password = password
 
-    def save_existing_credential_accounts(self):
-        User.user_list.append(self)
+#     def save_existing_credential_accounts(self):
+#         User.user_list.append(self)
 
-    @classmethod
-    def view_my_various_credential_accounts(cls):
-        return cls.user_list
+#     @classmethod
+#     def view_my_various_credential_accounts(cls,username):
+#         for user in cls.user_list:
+#             if user.username == username:
+#                 return cls.user_list
 
-    def delete_credential_account(self):
-        User.user_list.remove(self)
+#     def delete_credential_account(self):
+#         User.user_list.remove(self)
 
 
     
--- user_test.py
+++ user_test.py
@@ -17,5 +17,38 @@ class TestUser(unittest.TestCase):
         self.new_user.save_created_credential() 
         self.assertEqual(len(User.user_list),1)  
 
+    def test_save_multiple_user(self):
+        self.new_user.save_created_credential()
+        test_user = User("Test","user") 
+        test_user.save_created_credential()
+        self.assertEqual(len(User.user_list),2)
+
+    def tearDown(self):
+        
+        User.user_list = []
+
+    def delete_user(self):
+
+        User.user_list.remove(self)
+
+    
+    def test_delete_contact(self):
+        
+        self.new_user.save_created_credential()
+        test_user = User("Test","user")
+        test_user.save_created_credential()
+
+        self.new_user.delete_credential_account()
+        self.assertEqual(len(User.user_list),1)
+
+    def test_find_user_by_username(self):
+        
+        self.new_user.save_created_credential()
+        test_user = User("Test","user")
+        test_user.save_created_credential()
+
+        found_user = User.view_my_various_credential_accounts("Test")
+
+        self.assertEqual(found_user.username,test_user.username)
 if __name__ == '__main__':
     unittest.main()
